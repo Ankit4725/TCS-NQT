0f65fcca5e879815402037c32c18cb9693ce66cc
flying wizard <100211110+Ankit4725@users.noreply.github.com>
2023-03-29 10:09:05 +0530

    Create Problem1.cpp

---
+++ Number System/Problem1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std ;
+void binary_decimal(int number)
+{
+    int r ;
+    int result ;
+    int sum = 0 ;
+    int i = 0 ;
+    while(number > 0)
+    {
+        r = number%10 ;
+        number = number/10 ;
+        result = r*pow(2,i) ;
+        i++ ;
+        sum = sum + result ;
+    }
+    cout<<sum ;
+}
+int main()
+{
+    int number ;
+    cout<<"Enter the number " ;
+    cin>>number ;
+    binary_decimal(number) ;
+}
+
+/* For mathematical logics you can refer this site */ :-  https://www.cuemath.com/numbers/binary-to-decimal/
