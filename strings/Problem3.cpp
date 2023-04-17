56dc95ff8011ac553cdbf7012ad8950c0b273792
flying wizard <100211110+Ankit4725@users.noreply.github.com>
2023-04-17 09:19:26 +0530

    Create Problem3.cpp

---
+++ strings/Problem3.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    char alphabet ;
+    cout<<"Enter alphabet " ;
+    cin>>alphabet ;
+    int ascii ;
+    ascii = (int)alphabet ;
+    cout<<ascii ;
+}
